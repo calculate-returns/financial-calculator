c933f69653e2f74e2506d8eaef8d81673e6e1ab8
sOubHik <soubhik93@gmail.com>
2023-06-23 17:27:27 +0530

    Initial Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,58 @@ void main() {
   runApp(const MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const calculators = ['CAGR','Absolute Return','Real Estate','Commercial','XIRR','Loan'];
+  static const calculators = ['CAGR','Absolute Return','Real Estate','Commercial','XIRR','Loan', "Break FD", "How much Downpayment"];
   @override
   Widget build(BuildContext context) {
+
+    var appTheme = ThemeData(
+        useMaterial3: true,
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
+        //backgroundColor : Colors.indigo,
+        cardColor: const Color(0xffcdf1ee),
+        appBarTheme: const AppBarTheme(
+          elevation: 10,
+          backgroundColor: Color(0xff1e948b),
+          centerTitle: false,
+          titleTextStyle: TextStyle(color: Colors.black87, fontSize: 30, fontWeight: FontWeight.normal,  fontFamily: "Helvetica"),
+          surfaceTintColor: Color(0xffcdf1ee),
+
+        )
+    );
+
     return MaterialApp(
       title: 'Calculate the numbers',
+      theme: appTheme,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Calculate your numbers'),
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.grey,
-            elevation: 10
+          title: Text('Financial Calculator',
+            style: Theme.of(context).appBarTheme.titleTextStyle)
         ),
         body: GridView.count(
+          scrollDirection: Axis.vertical,
           crossAxisCount: 2 ,
-          children: List.generate(6,(index){
+          children: List.generate(8,(index){
             return Card(
               elevation: 10,
+              margin: const EdgeInsets.fromLTRB(12, 20 , 12 , 10),
+              surfaceTintColor: const Color(0xff1e948b),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
+                borderRadius: BorderRadius.circular(25.0),
+                side: const BorderSide(color: Color(0xff1e948b) , width: 2)
               ),
-              color: Colors.grey,
+            //  color: Colors.grey,
               child: Center(
                   child: Text(
                       calculators[index],
                       textAlign: TextAlign.center,
                       style: const TextStyle(
-                        fontSize: 30,
-                        fontFamily: "Roboto"
+                        fontSize: 22,
+                        fontFamily: "Helvetica",
+                        fontWeight: FontWeight.normal
                       )
                   )
               ),
@@ -45,40 +67,6 @@ class MyApp extends StatelessWidget {
           }),
         ),
       ),
-    );
-  }
-}
-
-
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      },
     );
   }
 }
